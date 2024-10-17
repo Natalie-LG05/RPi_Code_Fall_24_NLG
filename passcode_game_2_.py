8ad7b035c9ee9aa209b6615d9a16350f50876c4b
Natalie <christiantylergates@gmail.com>
2024-10-17 00:31:50 -0500

    exit handling

--- passcode_game_2_.py
+++ passcode_game_2_.py
@@ -30,37 +30,46 @@ GPIO.setmode(GPIO.BCM)
 
 leds = {
     # First set of LEDs
-    "LED_G1": led(4),  # green
-    "LED_B1": led(5),  # blue
-    "LED_Y1": led(6),  # yellow
-    "LED_R1": led(12), # red
+    'LED_G1': led(4),  # green
+    'LED_B1': led(5),  # blue
+    'LED_Y1': led(6),  # yellow
+    'LED_R1': led(12), # red
 
-    "LED_G3": led(13), # green
-    "LED_R2": led(16), # red
-    "LED_Y2": led(27), # yellow
-    "LED_B2": led(26), # blue
+    'LED_G3': led(13), # green
+    'LED_R2': led(16), # red
+    'LED_Y2': led(27), # yellow
+    'LED_B2': led(26), # blue
 }
 
 buttons = {
 # First set of LEDs
-    "BUTTON_1": button(23),  # green
-    "BUTTON_2": button(22),  # blue
-    "BUTTON_3": button(21),  # yellow
-    "BUTTON_4": button(20), # red
+    'BUTTON_1': button(23),  # green
+    'BUTTON_2': button(22),  # blue
+    'BUTTON_3': button(21),  # yellow
+    'BUTTON_4': button(20), # red
 
-    "BUTTON_5": button(19), # green
-    "BUTTON_6": button(18), # red
+    'BUTTON_5': button(19), # green
+    'BUTTON_6': button(18), # red
 }
 
 # Exit handler
 @atexit.register
 def on_exit():
-    print("Closing!")
+    print('Closing!')
+    for led_ in leds.values():
+        led_.off()
     GPIO.cleanup()
 
 # Main Code
-print("Starting!")
+print('Starting!')
 
 while True:
-    if buttons["BUTTON_1"].is_pressed():
-        exit()
+    # To turn off the program cleanly (without having to force quit it or keyboard interrupt etc.)
+    # Triggers if at least 4 buttons are pressed at once
+    i = 1
+    for button_ in buttons.values():
+        if button_.is_pressed():
+            i += 1
+        if i >= 4:
+            exit()
+        break
